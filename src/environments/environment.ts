46956c3812c5cbccbafb01726c89231ee57971dd
Your Name <you@example.com>
2025-02-06 17:21:16 -0500

    Solucion 3 en web

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
     //HOST: 'http://localhost:8080/api/v1',  
     //paypalReturnUrl: 'http://localhost:4200/pages/reservation/form'
 
-    HOST: 'https://restaurant-reservation-api-kv05.onrender.com/api/v1',
+    HOST: 'https://restaurant-reservation-api-z6ni.onrender.com/api/v1',
     paypalReturnUrl: 'https://edteamreserve.netlify.app/pages/reservation/form'
     
 };
