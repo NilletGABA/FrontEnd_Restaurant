a0ad0673fa5392c9f794623d9a3ec76534194c8c
Your Name <you@example.com>
2025-08-07 15:36:34 -0500

    Descripción de los cambios realizados

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
    
-    //HOST: 'http://localhost:8080/api/v1',  
-    //paypalReturnUrl: 'http://localhost:4200/pages/reservation/form'
+    HOST: 'http://localhost:8080/api/v1',  
+    paypalReturnUrl: 'http://localhost:4200/pages/reservation/form'
 
-    HOST: 'https://restaurant-reservation-api-z6ni.onrender.com/api/v1',
-    paypalReturnUrl: 'https://raulgb.netlify.app/pages/reservation/form'
+    //HOST: 'https://restaurant-reservation-api-z6ni.onrender.com/api/v1',
+    //paypalReturnUrl: 'https://raulgb.netlify.app/pages/reservation/form'
     
 };
