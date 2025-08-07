73697e7a2cae54d6d884c80abafe48ce2b54ab62
Your Name <you@example.com>
2025-08-07 17:32:56 -0500

    Ultima actualizacion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
 // paypalReturnUrl: 'http://localhost:4200/pages/reservation/form'
 
 HOST: 'https://api-restaurant-t64l.onrender.com/api/v1',
-paypalReturnUrl: 'https://raulgb.netlify.app/pages/reservation/form'
+paypalReturnUrl: 'https://nillet-reserve.netlify.app/pages/reservation/form'
     
 };
